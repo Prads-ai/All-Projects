05ecd5006d8baef249325331de8ff3e370745ce8
DPach <63697168+Prads-ai@users.noreply.github.com>
2022-09-05 22:26:10 -0400

    Update Leap-Year.py
    >on every year that is evenly divisible by 4
    >  **except** every year that is evenly divisible by 100
    > **unless** the year is also evenly divisible by 400`
    
    e.g. The year 2000:
    
    2000 ÷ 4 = 500 (Leap)
    
    2000 ÷ 100 = 20 (Not Leap)
    
    2000 ÷ 400 = 5 (Leap!)
    
    So the year 2000 is a leap year.
    
    But the year 2100 is not a leap year because:
    
    2100 ÷  4 = 525 (Leap)
    
    2100 ÷ 100 = 21 (Not Leap)
    
    2100 ÷ 400 = 5.25 (Not Leap)
    
    **Warning** your output should match the Example Output format exactly, even the positions of the commas and full stops. 
    
    # Example Input 1
    
    ```
    2400
    ```
    
    # Example Output 1
    
    ```
    Leap year.
    ```
    
    # Example Input 2
    
    ```
    1989
    ```
    
    # Example Output 2
    
    ```
    Not leap year.
    ```
    
    e.g. When you hit **run**, this is what should happen:  
    
     ![](https://cdn.fs.teachablecdn.com/AthNqKoSm6JD4sMom2X2)
    
    # Hint
    
    1. Try to visualise the rules by creating a flow chart on www.draw.io
    2. If you really get stuck, you can see the flow chart I created: 
    
    https://bit.ly/36BjS2D
    
    # Test Your Code
    
    Before checking the solution, try copy-pasting your code into this repl: 
    
    [https://repl.it/@appbrewery/day-3-3-test-your-code](https://repl.it/@appbrewery/day-3-3-test-your-code)
    
    This repl includes my testing code that will check if your code meets this assignment's objectives.

--- Python/100 projects in 100 days/Day 3/Control Flow And Logical/Leap-Year.py
+++ Python/100 projects in 100 days/Day 3/Control Flow And Logical/Leap-Year.py
@@ -1,1 +1,27 @@
 
+#>on every year that is evenly divisible by 4
+#>**except** every year that is evenly divisible by 100
+#>**unless** the year is also evenly divisible by 400`
+
+def main():
+     year = input("Enter a year: ")
+     # validating
+     
+     if year.isnumeric() :
+         # Converting year to an integer
+         year = int(year)
+         if year % 4 == 0:
+             if year % 100 == 0:
+                 if year % 400 == 0:
+                     print("Leap Year")
+                 else:
+                     print("not a leap year")
+             else:
+                 print("not a leap year")
+         else:
+             print("not a leap year")
+     else :
+        print("This is not a valid year")
+
+main()
+
